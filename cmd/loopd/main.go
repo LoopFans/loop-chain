a8abb9cd0443192dd5e2392c998deeb766c86b5b
Reece Williams <31943163+Reecepbcups@users.noreply.github.com>
2024-03-26 22:20:51 -0500

    add loop chain to the repo (#1)
    * init files
    
    * temp for private testnet
    
    * use loop-chain for goreleaser

---
+++ cmd/loopd/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"os"
+
+	"cosmossdk.io/log"
+
+	"github.com/LoopFans/loop-chain/app"
+	svrcmd "github.com/cosmos/cosmos-sdk/server/cmd"
+)
+
+func main() {
+	rootCmd := NewRootCmd()
+
+	if err := svrcmd.Execute(rootCmd, "", app.DefaultNodeHome); err != nil {
+		log.NewLogger(rootCmd.OutOrStderr()).Error("failure when running app", "err", err)
+		os.Exit(1)
+	}
+}
